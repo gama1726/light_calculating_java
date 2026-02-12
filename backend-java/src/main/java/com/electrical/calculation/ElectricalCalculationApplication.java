93dabbb14ff930ba464f15c26f25d28676a41940
gama1726 <gama17one@gmail.com>
2026-02-13 02:56:54 +0300

    feat(backend): Maven и Spring Boot

---
+++ backend-java/src/main/java/com/electrical/calculation/ElectricalCalculationApplication.java
@@ -0,0 +1,12 @@
+package com.electrical.calculation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricalCalculationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ElectricalCalculationApplication.class, args);
+    }
+}
